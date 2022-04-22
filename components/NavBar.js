c616fe77c2b0cb54c93125645d34839d330b87fc
YoonMinSoo <minsoo9615@naver.com>
2022-04-22 12:02:02 +0900

    2.1 Fetching Data

--- components/NavBar.js
+++ components/NavBar.js
@@ -7,6 +7,7 @@ export default function NavBar(){
 
     // return <nav className={styles.nav}>
     return <nav>
+        <img src="/vercel.svg" />
         <Link href="/">
             {/* <a className={router.pathname==="/" ? styles.active : ""}>Home</a> */}
             <a className={router.pathname==="/" ? "active" : ""}>Home</a>
@@ -16,13 +17,32 @@ export default function NavBar(){
             <a className={router.pathname==="/about" ? "active" : ""}>About</a>
         </Link>
         <style jsx>{`
-            a{
-                text-decoration : none;
-            }
-            .active{
-                color : red;
-            }
-        `}</style>
+        nav {
+          display: flex;
+          gap: 10px;
+          flex-direction: column;
+          align-items: center;
+          padding-top: 20px;
+          padding-bottom: 10px;
+          box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+            rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
+        }
+        img {
+          max-width: 100px;
+          margin-bottom: 5px;
+        }
+        nav a {
+          font-weight: 600;
+          font-size: 18px;
+        }
+        .active {
+          color: tomato;
+        }
+        nav div {
+          display: flex;
+          gap: 10px;
+        }
+      `}</style>
         {/* <style jsx>{`
             nav{
                 background-color : tomato;
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,34 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import NavBar from "../components/NavBar";
 import Seo from "../components/Seo";
 
+const API_KEY = "111dfd2e6256cf43d74f1bf2b8811c4b";
 export default function Home(){
     
     const [counter, setCounter] = useState(0);
-
+    const [movies, setMovies] = useState();
+    
+    useEffect(()=>{
+        (async () => {
+            const {results} = await(
+                await fetch(
+                    `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+                    )
+                ).json();
+            setMovies(results);
+            }
+        )(); //?? 무슨문법이야
+    },[]);
     return (
     <div>
         {/* <NavBar/> */}
         <Seo title="Home"/>
-        <h1>Hello{counter}</h1>
-        <button onClick={() =>{setCounter(prev=>prev+1);}}>+</button>
+        {!movies && <h4>Loading...</h4>}
+        {movies?.map(movie => ( //movie가 존재하면 map실행
+        <div key={movie.id}>
+            <h4>{movie.original_title}</h4>
+        </div>
+        ))}
         {/* <style jsx ocglobal>{`
             a{
                 color: orange;
